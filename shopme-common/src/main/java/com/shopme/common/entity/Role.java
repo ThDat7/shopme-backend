55ed4bb9cfbdc334b00280f3722172f43e5c14e6
ThDat7 <mailreport10@gmail.com>
2025-03-11 13:34:54 +0700

    feat: add Role and User entities, update project configuration
    - Created Role and User JPA entities in shopme-common module
    - Added Lombok dependency to common and webparent modules
    - Added repositories for Role and User in admin module
    - Updated Maven compiler plugin configuration
    - Updated Java version to 17 in project properties

---
+++ shopme-common/src/main/java/com/shopme/common/entity/Role.java
@@ -0,0 +1,23 @@
+package com.shopme.common.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 40, nullable = false, unique = true)
+	private String name;
+	
+	@Column(length = 150, nullable = false)
+	private String description;
+}
---
+++ shopme-common/src/main/java/com/shopme/common/entity/User.java
@@ -0,0 +1,46 @@
+package com.shopme.common.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 128, nullable = false, unique = true)
+	private String email;
+	
+	@Column(length = 64, nullable = false)
+	private String password;
+	
+	@Column(name = "first_name", length = 45, nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name", length = 45, nullable = false)
+	private String lastName;
+	
+	@Column(length = 64)
+	private String photos;
+	
+	private boolean enabled;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(
+			name = "users_roles",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id")
+			)
+	private Set<Role> roles = new HashSet<>();
+}
