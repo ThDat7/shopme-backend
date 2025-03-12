206a82e2a613d9a9147b12db610a1dd48d4a54fd
ThDat7 <mailreport10@gmail.com>
2025-03-12 16:50:56 +0700

    feat: add Category entity and repository
    - Create Category JPA entity with relationships and annotations
    - Implement CategoryRepository for database operations

---
+++ shopme-common/src/main/java/com/shopme/common/entity/Category.java
@@ -0,0 +1,40 @@
+package com.shopme.common.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(length = 128, nullable = false, unique = true)
+	private String name;
+
+	@Column(length = 64, nullable = false, unique = true)
+	private String alias;
+
+	@Column(length = 128, nullable = false)
+	private String image;
+
+	private boolean enabled;
+
+	@ManyToOne
+	@JoinColumn(name = "parent_id")
+	private Category parent;
+	
+	@OneToMany(mappedBy = "parent")
+	@OrderBy("name asc")
+	private Set<Category> children = new HashSet<>();
+}
