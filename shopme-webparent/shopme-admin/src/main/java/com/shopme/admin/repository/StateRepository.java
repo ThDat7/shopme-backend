77906b8315398ba4b486ee47d968d3142c7e1e74
ThDat7 <mailreport10@gmail.com>
2025-03-18 20:19:30 +0700

    feat: add Country and State entities with repositories
    - Create Country and State JPA entities with necessary fields and relationships
    - Implement CountryRepository and StateRepository for database operations

---
+++ shopme-webparent/shopme-admin/src/main/java/com/shopme/admin/repository/StateRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.admin.repository;
+
+import com.shopme.common.entity.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StateRepository extends JpaRepository<State, Integer> {
+}
