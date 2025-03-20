a848c1149147dab42b3897e9dfd31e17ef3528ba
ThDat7 <mailreport10@gmail.com>
2025-03-20 23:20:53 +0700

    feat: add Address entity and repository for customer address management
    - Create Address entity
    - Implement AddressRepository for database operations

---
+++ shopme-common/src/main/java/com/shopme/common/entity/Address.java
@@ -0,0 +1,55 @@
+package com.shopme.common.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "addresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "first_name", nullable = false, length = 45)
+	private String firstName;
+
+	@Column(name = "last_name", nullable = false, length = 45)
+	private String lastName;
+
+	@Column(name = "phone_number", nullable = false, length = 15)
+	private String phoneNumber;
+
+	@Column(name = "address_line_1", nullable = false, length = 64)
+	private String addressLine1;
+
+	@Column(name = "address_line_2", length = 64)
+	private String addressLine2;
+
+	@Column(nullable = false, length = 45)
+	private String city;
+
+	@Column(nullable = false, length = 45)
+	private String state;
+
+	@Column(name = "postal_code", nullable = false, length = 10)
+	private String postalCode;
+
+	@ManyToOne
+	@JoinColumn(name = "country_id", nullable = false)
+	private Country country;
+	
+	@ManyToOne
+	@JoinColumn(name = "customer_id", nullable = false)
+	private Customer customer;
+	
+	@Column(name = "default_address")
+	private boolean defaultForShipping;
+	
+}
---
+++ shopme-webparent/shopme-client/src/main/java/com/shopme/client/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.client.repository;
+
+import com.shopme.common.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+}
