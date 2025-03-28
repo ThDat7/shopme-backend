d5df0fbe2e5475b1b054da96461a57c3f6fba61c
ThDat7 <mailreport10@gmail.com>
2025-03-28 18:55:46 +0700

    feat: update config for multi enviroment.
    - Update application.properties to use profile config.
    - Introduce application-sample.properties for both admin and client for sample configurations.
    - Update WebConfig to use frontend.host property for CORS allowed origins.
    - Add application-dev.properties to .gitignore to prevent tracking of environment-specific configurations.
    - Enhance resource handling in WebConfig by exposing additional directories.

--- shopme-webparent/shopme-admin/src/main/java/com/shopme/admin/config/WebConfig.java
+++ shopme-webparent/shopme-admin/src/main/java/com/shopme/admin/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -10,11 +11,16 @@ import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final String FRONTEND_HOST;
+
+    public WebConfig(@Value("frontend.host") String frontendHost) {
+        this.FRONTEND_HOST = frontendHost;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(FRONTEND_HOST)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
@@ -26,6 +32,7 @@ public class WebConfig implements WebMvcConfigurer {
         exposeDirectory("../category-images", registry);
         exposeDirectory("../brand-logos", registry);
         exposeDirectory("../product-images", registry);
+        exposeDirectory("../site-logo", registry);
     }
 
     private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
--- shopme-webparent/shopme-client/src/main/java/com/shopme/client/config/WebConfig.java
+++ shopme-webparent/shopme-client/src/main/java/com/shopme/client/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.shopme.client.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -10,11 +11,16 @@ import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final String FRONTEND_HOST;
+
+    public WebConfig(@Value("frontend.host") String frontendHost) {
+        this.FRONTEND_HOST = frontendHost;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(FRONTEND_HOST)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
