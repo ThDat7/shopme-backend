52e5e2d3544b67d978bf27d3cb881d0897f94138
ThDat7 <mailreport10@gmail.com>
2025-04-12 20:42:58 +0700

    feat: enhance WebSecurityConfig with CORS support and update public endpoints
    - Introduce new public resource endpoints for category and brand images.
    - Add CORS configuration to allow requests from the specified frontend host.
    - Refactor existing public and authenticated endpoint methods for improved clarity and security management.

--- shopme-webparent/shopme-client/src/main/java/com/shopme/client/config/security/WebSecurityConfig.java
+++ shopme-webparent/shopme-client/src/main/java/com/shopme/client/config/security/WebSecurityConfig.java
@@ -24,6 +24,9 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Collection;
@@ -40,10 +43,15 @@ public class WebSecurityConfig {
     @Value("${google.client.id}")
     private String googleClientId;
 
+    @Value("${frontend.host}")
+    private String FRONTEND_HOST;
+
     private final CustomerContextService customerContextService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
         http.oauth2ResourceServer(oauth2 ->
                 oauth2.jwt(jwtConfigurer ->
                         jwtConfigurer.decoder(jwtDecoder())
@@ -54,6 +62,9 @@ public class WebSecurityConfig {
                 request
                         // Public endpoints - no authentication required
                         .requestMatchers(publicEndpoints()).permitAll()
+
+                        // Public resource endpoints - no authentication required
+                        .requestMatchers(publicResourceEndpoint()).permitAll()
 
                         // Protected endpoints - authentication required
                         .requestMatchers(authenticatedEndpoints()).access(authorizationManager())
@@ -67,17 +78,30 @@ public class WebSecurityConfig {
 
     private String[] publicEndpoints() {
         return new String[]{
+                "/api/v1/settings/**",
                 "/api/v1/auth/**",
+                "/api/v1/carousel-images/**",
                 "/api/v1/customers/register",
                 "/api/v1/customers/verify",
                 "/api/v1/customers/resend-verification",
                 "/api/v1/products/**",
                 "/api/v1/categories/**",
                 "/api/v1/brands/**",
+                "/api/v1/feature-category-brand/**",
                 "/api/v1/promotions/**",
                 "/api/v1/feature-categories/**",
                 "/api/v1/locations/**",
-                "/api/v1/reviews//product/**"
+                "/api/v1/reviews//product/**",
+                "/api/v1/checkout/payment/PAY_OS/handler"
+        };
+    }
+
+    private String[] publicResourceEndpoint() {
+        return new String[]{
+                "/category-images/**",
+                "/brand-logos/**",
+                "/product-images/**",
+                "/carousel-images/**",
         };
     }
 
@@ -88,9 +112,24 @@ public class WebSecurityConfig {
                 "/api/v1/addresses/**",
                 "/api/v1/cart/**",
                 "/api/v1/orders/**",
-                "/api/v1/checkout/**",
+                "/api/v1/checkout/calculate-shipping",
+                "/api/v1/checkout/payment/COD",
+                "/api/v1/checkout/payment/PAY_OS",
                 "/api/v1/reviews/order-detail/**"
         };
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(FRONTEND_HOST)); // Sử dụng FRONTEND_HOST từ WebConfig
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
